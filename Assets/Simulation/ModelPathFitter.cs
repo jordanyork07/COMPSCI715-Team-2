2c39c9e9b8e5ad235d8c75df7d4440b0955a6d2b
Matan Yosef <matan.yosef02@gmail.com>
2023-09-10 22:20:57 +1200

    fitter: added variability for the platforms

--- Assets/Simulation/ModelPathFitter.cs
+++ Assets/Simulation/ModelPathFitter.cs
@@ -12,8 +12,14 @@ public class ModelPathFitter : PathFitter
 
 	protected override GameObject CreateModel()
 	{
+        // Create an array of prefab names
+        string[] prefabNames = { "SM_Env_DirtMound_01", "SM_Env_DirtMound_02", "SM_Env_DirtMound_03", "SM_Env_DirtMound_04", "SM_Env_DirtMound_05" };
+
+        // Randomly select a prefab name from the array
+        string selectedPrefabName = prefabNames[Random.Range(0, prefabNames.Length)];
+
         // Load the "SM_Env_DirtMount_01" prefab from the PolygonAdventure asset
-        GameObject modelPrefab = Resources.Load<GameObject>("SM_Env_DirtMound_01");
+        GameObject modelPrefab = Resources.Load<GameObject>(selectedPrefabName);
 
         if (modelPrefab != null)
         {
